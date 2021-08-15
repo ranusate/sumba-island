ae76dde0945cb99c20c683dbc446f2434ca4a77b
Ranus Ate <radianus.ra@mbteknologi.com>
2021-08-15 22:50:50 +0700

    update ui

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -14,11 +15,12 @@ class RegisterController extends Controller
             'active' => 'register'
         ]);
     }
+
     public function indexs()
     {
         return view('login.index', [
-            'title' => 'Register',
-            'active' => 'register'
+            'title' => 'Login',
+            'active' => 'login'
         ]);
     }
 
@@ -26,12 +28,13 @@ class RegisterController extends Controller
     {
         $validateData =  $request->validate([
             'name' => 'required|min:3|max:255',
-            'username' => 'required|min:3|max:255|unique:users,username',
+            'username' => 'required|min:3|max:255|unique:users',
             'email' => 'required|email:rfc,dns|min:3|max:255|unique:users',
             'password' => 'required|min:3|max:255',
         ]);
+        $validateData['password'] = Hash::make($request->username);
         $request->session()->flash('success', 'Register successfull! Please login');
         User::create($validateData);
-        return redirect('/login');
+        return redirect('/logins');
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,7 +1,7 @@
-
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -17,68 +17,101 @@
     <!-- Style -->
     <link rel="stylesheet" href="/login/css/style.css">
 
-    <title>Login #8</title>
-  </head>
-  <body>
 
-  <div class="content">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 order-md-2">
-          <img src="/login/images/undraw_file_sync_ot38.svg" alt="Image" class="img-fluid">
-        </div>
-        <div class="col-md-6 contents">
-          <div class="row justify-content-center">
-            <div class="col-md-8">
-              <div class="mb-4">
-              <h3>Sign In to <strong>Colorlib</strong></h3>
-              <p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p>
+    <!-- boostrap -->
+
+    <title>{{$title}}</title>
+</head>
+
+<body>
+
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 order-md-2">
+                    <img src="/login/images/undraw_file_sync_ot38.svg" alt="Image" class="img-fluid">
+                </div>
+                <div class="col-md-6 contents">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="mb-4">
+                                <h3>Please Register</h3>
+                            </div>
+                            <form action="/register" method="post">
+                                @csrf
+                                <div class="form-group first">
+                                    <label for="Name">Name</label>
+                                    <input type="text" class="form-control is-invalid" name="name" value="{{old('name')}}">
+                                    @error('name')
+                                    <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                        {{ $message   }}
+                                    </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input type="text" class="form-control is-invalid" name="username"  value="{{old('username')}}">
+                                    @error('username')
+                                    <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                        {{ $message   }}
+                                    </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control is-invalid" name="email"  value="{{old('email')}}">
+                                    @error('email')
+                                    <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                        {{ $message   }}
+                                    </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group last mb-4">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control is-invalid" name="password">
+                                    @error('password')
+                                    <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                        {{ $message   }}
+                                    </div>
+                                    @enderror
+                                </div>
+
+                                <div class="d-flex mb-5 align-items-center">
+                                    <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
+                                        <input type="checkbox" checked="checked" />
+                                        <div class="control__indicator"></div>
+                                    </label>
+                                    <span class="ml-auto">
+                                        <a href="/logins" class="forgot-pass">Login</a>
+                                    </span>
+                                </div>
+                                <input type="submit" value="Register" class="btn text-white btn-block btn-primary">
+
+                                <span class="d-block text-left my-4 text-muted"> or sign in with</span>
+
+                                <div class="social-login">
+                                    <a href="#" class="facebook">
+                                        <span class="icon-facebook mr-3"></span>
+                                    </a>
+                                    <a href="#" class="twitter">
+                                        <span class="icon-twitter mr-3"></span>
+                                    </a>
+                                    <a href="#" class="google">
+                                        <span class="icon-google mr-3"></span>
+                                    </a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <form action="#" method="post">
-              <div class="form-group first">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" id="username">
-
-              </div>
-              <div class="form-group last mb-4">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password">
-
-              </div>
-
-              <div class="d-flex mb-5 align-items-center">
-                <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
-                  <input type="checkbox" checked="checked"/>
-                  <div class="control__indicator"></div>
-                </label>
-                <span class="ml-auto"><a href="/logins" class="forgot-pass">Login</a></span>
-              </div>
-
-              <input type="submit" value="Log In" class="btn text-white btn-block btn-primary">
-
-              <span class="d-block text-left my-4 text-muted"> or sign in with</span>
-
-              <div class="social-login">
-                <a href="#" class="facebook">
-                  <span class="icon-facebook mr-3"></span>
-                </a>
-                <a href="#" class="twitter">
-                  <span class="icon-twitter mr-3"></span>
-                </a>
-                <a href="#" class="google">
-                  <span class="icon-google mr-3"></span>
-                </a>
-              </div>
-            </form>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
+    <!-- Boostrap -->
     <script src="/login/js/jquery-3.3.1.min.js"></script>
     <script src="/login/js/popper.min.js"></script>
     <script src="/login/js/bootstrap.min.js"></script>
     <script src="/login/js/main.js"></script>
-  </body>
+</body>
+
 </html>
