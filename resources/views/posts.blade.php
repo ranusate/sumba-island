2bd4e68e5fc34c9fdd3390047a5f3dfc25b75d38
Ranus Ate <radianus.ra@mbteknologi.com>
2021-08-24 01:06:29 +0700

    change nav about

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-3 text-center"> {{$title}}</h1>
-
+<h1 class="mb-5 text-center"> {{$title}}</h1>
 <div class="row justify-content-center mb-3">
     <div class="col-md-6">
         <form action="/posts">
