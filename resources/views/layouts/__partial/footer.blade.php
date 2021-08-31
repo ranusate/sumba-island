bf10360251a723ff5efaa16b259db219f505d08a
Ranus Ate <radianus.ra@mbteknologi.com>
2021-08-31 13:34:07 +0700

    update footer

--- resources/views/layouts/__partial/footer.blade.php
+++ resources/views/layouts/__partial/footer.blade.php
@@ -2,40 +2,9 @@
     <!-- Footer -->
     <footer class="text-center text-lg-start bg-light text-muted bg-dark mt-4">
         <!-- Section: Social media -->
-        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-            <!-- Left -->
-            <div class="me-5 d-none d-lg-block">
-                <span>Get connected with us on social networks:</span>
-            </div>
+        <section class="d-flex justify-content-center justify-content-lg-between p-4">
             <!-- Left -->
 
-            <!-- Right -->
-            <div class="text-light">
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-google"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-github"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-whatsapp"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-telegram"></i>
-                </a>
-            </div>
             <!-- Right -->
         </section>
         <!-- Section: Social media -->
