156ff4f6449e918c438036b6fb51933229f3e0b5
Ranus Ate <radianus.ra@mbteknologi.com>
2021-09-25 11:48:30 +0700

    update navbar

--- resources/views/layouts/__partial/navbar.blade.php
+++ resources/views/layouts/__partial/navbar.blade.php
@@ -1,22 +1,22 @@
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar fixed-top navbar-expand-lg shadow p-3 mb-5 bg-white rounded ">
     <div class="container">
-        <a class="navbar-brand" href="/">{{env('APP_NAME')}}</a>
+        <a class="navbar-brand text-reset" href="/">{{env('APP_NAME')}}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+        <div class="collapse navbar-collapse " id="navbarNav">
+            <ul class="navbar-nav text-reset">
                 <li class="nav-item">
-                    <a class="nav-link {{ \Illuminate\Support\Facades\Request::is('/') ? 'active' :'' }}" aria-current="page" href="/">Home</a>
+                    <a class="nav-link {{ \Illuminate\Support\Facades\Request::is('/') ? 'active' :'' }} text-reset" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link  {{\Illuminate\Support\Facades\Request::is('posts') ? 'active' :'' }}" href="/posts">Blog</a>
+                    <a class="nav-link  {{\Illuminate\Support\Facades\Request::is('posts') ? 'active' :'' }} text-reset" href="/posts">Blog</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link  {{ \Illuminate\Support\Facades\Request::is('categories') ? 'active' :'' }}" href="/categories">Categories</a>
+                    <a class="nav-link  {{ \Illuminate\Support\Facades\Request::is('categories') ? 'active' :'' }} text-reset" href="/categories">Categories</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link  {{\Illuminate\Support\Facades\Request::is('about') ? 'active' :'' }}" href="/about">About</a>
+                    <a class="nav-link  {{\Illuminate\Support\Facades\Request::is('about') ? 'active' :'' }} text-reset" href="/about">About</a>
                 </li>
             </ul>
 
@@ -46,7 +46,7 @@
                 </li>
                 @else
                 <li class="nav-item">
-                    <a href="/auth" class="nav-link  {{ \Illuminate\Support\Facades\Request::is('login' )? 'active' :'' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+                    <a href="/auth" class=" text-reset nav-link  {{ \Illuminate\Support\Facades\Request::is('login' )? 'active' :'' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
                 </li>
                 @endauth
             </ul>
