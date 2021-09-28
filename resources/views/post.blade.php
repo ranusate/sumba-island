cd0555b6b984278b435a9e3f99669b0aa8c50c42
Ranus Ate <radianus.ra@mbteknologi.com>
2021-09-28 12:13:21 +0700

    update view

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -15,7 +15,6 @@
                 <img src="{{asset('storage/'. $post->image)}}" alt="">
                 @else
                 <img src="https://source.unsplash.com/1200x400?{{$post->category->name}}" alt="{{$post->category->name}}" class="img-fluid">
-                @endif
             </div>
 
             <article class="my-3 fs-4">
